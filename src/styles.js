006c53d0e1b06355f5d4198ea82c6126da1cfb57
James Smith <james.smith@djalbat.com>
2021-07-12 21:53:54 +0100

    Slight improvements to the styles.

--- src/styles.js
+++ src/styles.js
@@ -3,7 +3,7 @@
 export const toggleSVGHeight = "2rem";
 export const entryItemMinHeight = "2rem";
 export const entryItemSVGHeight = "2rem";
-export const entryItemMarginTop = "2rem";
+export const entryItemMarginTop = "1.25rem";
 export const rubbishBinSVGHeight = "5rem";
 export const entriesListMarginLeft = "2rem";
 export const dragEntryItemFontSize = "1.2rem";
