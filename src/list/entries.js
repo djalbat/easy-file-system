18f0e87206bca990880fc7a32be311bcbcda54d8
James Smith <james.smith@djalbat.com>
2023-10-07 10:50:50 +0100

    Exposed the isTopmost() method.

--- src/list/entries.js
+++ src/list/entries.js
@@ -610,6 +610,7 @@ class EntriesList extends Element {
           collapseEntriesList = this.collapse.bind(this),  ///
           isEntriesListCollapsed = this.isCollapsed.bind(this), ///
           isEmpty = this.isEmpty.bind(this),
+          isTopmost = this.isTopmost.bind(this),
           addMarker = this.addMarker.bind(this),
           selectPath = this.selectPath.bind(this),
           addFilePath = this.addFilePath.bind(this),
@@ -635,6 +636,7 @@ class EntriesList extends Element {
       collapseEntriesList,
       isEntriesListCollapsed,
       isEmpty,
+      isTopmost,
       addMarker,
       selectPath,
 			addFilePath,
