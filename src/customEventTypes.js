0747fbe91c41ea98284c7e110147d3d60da28c63
James Smith <james.smith@djalbat.com>
2024-02-01 20:29:01 +0000

    Propagated.

--- src/customEventTypes.js
+++ src/customEventTypes.js
@@ -1,13 +1,13 @@
 "use strict";
 
-export const OPEN_CUSTOM_EVENT_TYPE = "open-custom";
-export const MOVE_CUSTOM_EVENT_TYPE = "move-custom";
-export const CHANGE_CUSTOM_EVENT_TYPE = "change-custom";
-export const CANCEL_CUSTOM_EVENT_TYPE = "cancel-custom";
-export const SELECT_CUSTOM_EVENT_TYPE = "select-custom";
-export const RENAME_CUSTOM_EVENT_TYPE = "rename-custom";
-export const REMOVE_CUSTOM_EVENT_TYPE = "remove-custom";
-export const CREATE_CUSTOM_EVENT_TYPE = "create-custom";
+export const OPEN_CUSTOM_EVENT_TYPE = "open";
+export const MOVE_CUSTOM_EVENT_TYPE = "move";
+export const CHANGE_CUSTOM_EVENT_TYPE = "change";
+export const CANCEL_CUSTOM_EVENT_TYPE = "cancel";
+export const SELECT_CUSTOM_EVENT_TYPE = "select";
+export const RENAME_CUSTOM_EVENT_TYPE = "rename";
+export const REMOVE_CUSTOM_EVENT_TYPE = "remove";
+export const CREATE_CUSTOM_EVENT_TYPE = "create";
 
 export default {
   OPEN_CUSTOM_EVENT_TYPE,
